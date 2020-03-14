cca7482c8e52f18f349483aa8c400648c32e2555
James Walsh <jamesandersonwalsh@gmail.com>
2020-03-13 21:52:55 -0600

    Add discord greeting and notify command

--- src/discord.ts
+++ src/discord.ts
@@ -1,18 +1,16 @@
 import { Client, PartialMessage, Message } from 'discord.js'
 
+const CMD_PREFIX = '!'
+const MSG_PREFIX = `Beep Boop 👋🤖🤙`
+
 export async function getDiscordClient(): Promise<Client> {
   const client = new Client()
 
   client.once('ready', () => {
-    console.log('Successfully started the Discord Client')
+    console.info('Successfully started the Discord Client')
   })
 
-  client.on('message', msg => {
-    const parsedMessage = parseMessage(msg)
-    if (parsedMessage.includes('butler')) {
-      msg.channel?.send(`Welcome to Virtual Hangs ${msg.member?.user}`)
-    }
-  })
+  client.on('message', handleMessages)
 
   try {
     await client.login(process.env.DISCORD_BOT_TOKEN)
@@ -23,10 +21,26 @@ export async function getDiscordClient(): Promise<Client> {
   return client
 }
 
-function parseMessage(message: Message | PartialMessage): string {
-  if (message.content) {
-    return message?.content.toLowerCase()
-  } else {
-    return (message as unknown) as string
+
+function handleMessages(message:Message|PartialMessage) : void {
+  const msgIsGreeting = message.content?.includes('butler')
+  if (msgIsGreeting) {
+    message.channel?.send(`${MSG_PREFIX} - Welcome to Virtual Hangs ${message.member?.nickname}`)
   }
+
+  if (!message.content?.startsWith(CMD_PREFIX) || message.author?.bot) return
+  const args = message.content.slice(CMD_PREFIX.length).split(' ')
+  const command = args.shift()?.toLowerCase()
+  
+  if (command === Commands.Notify) {
+    if (args) {
+      message.channel?.send(`${MSG_PREFIX} - I'll notify your friends in slack that ${message.member?.nickname} says '${args.join(' ')}' :call_me: `)
+    } else {
+      message.channel?.send(`${MSG_PREFIX} - Hey ${message.member?.nickname}! I'll notify your friends in slack that you've come online :call_me:`)
+    }
+  }
+}
+
+enum Commands {
+  Notify = 'notify'
 }
